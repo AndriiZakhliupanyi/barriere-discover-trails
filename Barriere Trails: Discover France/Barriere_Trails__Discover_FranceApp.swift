0b87ace647b64b5558cfce5ce7358c7b0eec7c6f
Andrii Zakhliupanyi <andrii.zakhliupanyi@gmail.com>
2025-03-05 09:41:01 +0200

    Initial Commit

---
+++ Barriere Trails: Discover France/Barriere_Trails__Discover_FranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barriere_Trails__Discover_FranceApp.swift
+//  Barriere Trails: Discover France
+//
+//  Created by Andrii Zakhliupanyi on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Barriere_Trails__Discover_FranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
